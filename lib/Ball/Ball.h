f48fb7e7fad6569a109f38299094810b7b0a8701
= <95466399+rcj-hana@users.noreply.github.com>
2022-12-25 22:38:30 +0900

    ファイル再分割化

---
+++ lib/Ball/Ball.h
@@ -0,0 +1,8 @@
+#ifndef __BALL_H_
+#define __BALL_H
+
+extern int BallAngle;
+
+void IRUpdate();
+
+#endif
--- lib/Motor/Motor.cpp
+++ lib/Motor/Motor.cpp
@@ -3,7 +3,7 @@
 #include "IMU.h"
 
 int MotorPins[4][2] = {{4,5},{2,3},{6,7},{8,9}};
-int speed = 200;
+extern int speed = 100;
 
 Motor::Motor() {
    for(int i = 0; i < 4; i++) {
